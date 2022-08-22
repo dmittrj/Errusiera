80c4967c6efd1ec2ab129948ff30d4d09de60590
Дмитрий Балабанов <dmittrj@outlook.com>
2022-08-22 17:58:49 +0300

    C-Sharp version

---
+++ ErrusieraCs/Noun.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ErrusieraCs
+{
+	enum Cases
+	{
+		Nominative, Genetive, Dative, Accusative, Instrumental, Prepositional
+	};
+
+	enum Number
+	{
+		Singular, Paucal, Plural
+	};
+	class Noun
+    {
+		public string Word;
+		private Cases WordCase;
+		private Number WordNumber;
+		public Noun(string word_noun_only)
+		{
+			Word = word_noun_only;
+		}
+		public Noun(string word_noun_only, Cases noun_case, Number noun_number)
+		{
+			Word = word_noun_only;
+			WordCase = noun_case;
+			WordNumber = noun_number;
+		}
+		~Noun() { }
+
+		public void ChangeCase(Cases case_to)
+		{
+			if (case_to == WordCase) { return; }
+			ToNominative();
+			switch (case_to)
+			{
+				case Cases.Genetive:
+					break;
+				case Cases.Dative:
+					if (Word[^1] == 'й')
+					{
+						Word = Word[0..^1] + "ю";
+					}
+					break;
+				case Cases.Accusative:
+					break;
+				case Cases.Instrumental:
+					break;
+				case Cases.Prepositional:
+					break;
+				default:
+					break;
+			}
+		}
+
+		public override string ToString()
+		{
+			return Word;
+		}
+
+
+		private void ToNominative()
+        {
+
+        }
+	}
+}
--- ErrusieraCs/Program.cs
+++ ErrusieraCs/Program.cs
@@ -4,9 +4,18 @@ namespace ErrusieraCs
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
-        }
+			//This are some examples of using Errusiera
+
+			//Example 1. Enter your name
+			Console.Write("Введите ваше имя > ");
+			string my_name1;
+			my_name1 = Console.ReadLine();
+			Noun E_my_name_1 = new Noun(my_name1, Cases.Nominative, Number.Singular);
+			E_my_name_1.ChangeCase(Cases.Dative);
+			my_name1 = E_my_name_1.ToString();
+			Console.WriteLine(my_name1 + " нравится Errusiera");
+		}
     }
 }
