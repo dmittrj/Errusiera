bfa9c42150205069dc268ca196e1c709f3a7c885
Дмитрий Балабанов <dmittrj@outlook.com>
2022-08-23 18:44:25 +0300

    Fixed typo

--- ErrusieraCpp/main.cpp
+++ ErrusieraCpp/main.cpp
@@ -6,7 +6,7 @@ int main() {
 	setlocale(LC_ALL, "Russian");
 	system("chcp 1251");
 
-	//This are some examples of using Errusiera
+	//These are some examples of using Errusiera
 
 	//Example 1. Enter your name
 	std::cout << "Введите ваше имя > ";
@@ -16,6 +16,7 @@ int main() {
 	E_my_name_1.change_case(Cases::Dative);
 	my_name1 = E_my_name_1.to_string();
 	std::cout << my_name1 << " нравится Errusiera" << std::endl;
+
 
 	//Example 2. Wedding
 	std::cout << "Введите имя мужа > ";
