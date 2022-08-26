8918d024b40534e24c264ef8b2aa8ce0dfe275f3
Дмитрий Балабанов <dmittrj@outlook.com>
2022-08-26 09:59:49 +0300

    Some changes in main()

--- ErrusieraCpp/main.cpp
+++ ErrusieraCpp/main.cpp
@@ -9,26 +9,26 @@ int main() {
 	//These are some examples of using Errusiera
 
 	//Example 1. Enter your name
-	std::cout << "¬ведите ваше им€ > ";
+	std::cout << "\302\342\345\344\350\362\345 \342\340\370\345 \350\354\377 > ";
 	std::string my_name1;
 	std::cin >> my_name1;
 	Noun E_my_name_1(my_name1, Cases::Nominative, Number::Singular);
 	E_my_name_1.change_case(Cases::Dative);
 	my_name1 = E_my_name_1.to_string();
-	std::cout << my_name1 << " нравитс€ Errusiera" << std::endl;
+	std::cout << my_name1 << " \355\360\340\342\350\362\361\377 Errusiera\n" << std::endl;
 
 
 	//Example 2. Wedding
-	std::cout << "¬ведите им€ мужа > ";
+	std::cout << "\302\342\345\344\350\362\345 \350\354\377 \354\363\346\340 > ";
 	std::string husband_name2;
 	std::cin >> husband_name2;
-	std::cout << "¬ведите им€ жены > ";
+	std::cout << "\302\342\345\344\350\362\345 \350\354\377 \346\345\355\373 > ";
 	std::string wife_name2;
 	std::cin >> wife_name2;
 	Noun E_husband_name2(husband_name2, Cases::Nominative, Number::Singular);
 	Noun E_wife_name2(wife_name2, Cases::Nominative, Number::Singular);
-	std::cout << E_husband_name2.to_string() << " женат на " << E_wife_name2.change_case(Cases::Prepositional) << std::endl;
-	std::cout << E_wife_name2.change_case(Cases::Nominative) << " замужем за " << E_husband_name2.change_case(Cases::Instrumental) << std::endl;
+	std::cout << E_husband_name2.to_string() << " \346\345\355\340\362 \355\340 " << E_wife_name2.change_case(Cases::Prepositional) << std::endl;
+	std::cout << E_wife_name2.change_case(Cases::Nominative) << " \347\340\354\363\346\345\354 \347\340 " << E_husband_name2.change_case(Cases::Instrumental) << "\n" << std::endl;
 
 
 	//Example 3. Time
