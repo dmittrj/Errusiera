f7e4387a9a3525b97a9e6c16f2a69c92472f0aad
Dmitry Balabanov <dmittrj@outlook.com>
2022-08-26 07:59:16 +0300

    Update main.cpp

--- ErrusieraCpp/main.cpp
+++ ErrusieraCpp/main.cpp
@@ -9,36 +9,36 @@ int main() {
 	//These are some examples of using Errusiera
 
 	//Example 1. Enter your name
-	std::cout << "";
+	std::cout << "Введите ваше имя > ";
 	std::string my_name1;
 	std::cin >> my_name1;
 	Noun E_my_name_1(my_name1, Cases::Nominative, Number::Singular);
 	E_my_name_1.change_case(Cases::Dative);
 	my_name1 = E_my_name_1.to_string();
-	std::cout << my_name1 << "" << std::endl;
+	std::cout << my_name1 << " нравится Errusiera" << std::endl;
 
 
 	//Example 2. Wedding
-	std::cout << "";
+	std::cout << "Введите имя мужа > ";
 	std::string husband_name2;
 	std::cin >> husband_name2;
-	std::cout << "";
+	std::cout << "Введите имя жены > ";
 	std::string wife_name2;
 	std::cin >> wife_name2;
 	Noun E_husband_name2(husband_name2, Cases::Nominative, Number::Singular);
 	Noun E_wife_name2(wife_name2, Cases::Nominative, Number::Singular);
-	std::cout << E_husband_name2.to_string() << "" << E_wife_name2.change_case(Cases::Prepositional) << std::endl;
-	std::cout << E_wife_name2.change_case(Cases::Nominative) << "" << E_husband_name2.change_case(Cases::Instrumental) << std::endl;
+	std::cout << E_husband_name2.to_string() << " женат на " << E_wife_name2.change_case(Cases::Prepositional) << std::endl;
+	std::cout << E_wife_name2.change_case(Cases::Nominative) << " замужем за " << E_husband_name2.change_case(Cases::Instrumental) << std::endl;
 
 
 	//Example 3. Time
-	std::cout << "";
+	std::cout << "Введите время в формате ЧЧ:ММ > ";
 	std::string time3;
 	std::cin >> time3;
 	std::string hours3 = time3.substr(0, 2);
 	std::string minutes3 = time3.substr(3, 2);
-	Noun E_hours3("", Cases::Nominative, Number::Singular);
-	Noun E_minutes3("", Cases::Nominative, Number::Singular);
+	Noun E_hours3("час", Cases::Nominative, Number::Singular);
+	Noun E_minutes3("минута", Cases::Nominative, Number::Singular);
 	E_hours3.change_case(Cases::Genetive);
 	E_minutes3.change_case(Cases::Genetive);
 }
