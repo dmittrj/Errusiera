2d0f7dc937282db2ce851c74a8ceca959e6f5420
Дмитрий Балабанов <dmittrj@outlook.com>
2022-08-28 09:38:30 +0300

    Testing regular expressions

--- ErrusieraCs/Noun.cs
+++ ErrusieraCs/Noun.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace Errusiera
 {
-	// Errusiera for C# 1.0.0-beta6
+	// Errusiera for C# 1.0.0-alpha6-1
 	// Dmitry Balabanov | github.com/dmittrj/Errusiera
 
 	/// <summary>
